20a4c3d7b946825b6d6aebad37de00c313a08d70
wesleyvelloso <wmarques092@gmail.com>
2022-05-23 19:26:48 -0300

    Adicionadas atributos cartaoCredito e anuidadeCartao. Adicionados construtores

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,17 +1,28 @@
 public class ContaCorrente extends Conta {
 
+    private Integer cartaoCredito;
+    private Double anuidadeCartao = 12.0;
+
     public ContaCorrente(Cliente cliente) {
-        super(cliente, "Corrente");  // new
-    }
-    public ContaCorrente(Cliente cliente, Integer agencia) {
-        super(cliente,"Corrente", agencia);
+        super(cliente);
     }
 
+    public ContaCorrente(Cliente cliente, Integer agencia) {
+        super(cliente, agencia);
+    }
+
+    public ContaCorrente(Cliente cliente, Integer agencia, Integer cartaoCredito) {
+        super(cliente, agencia);
+        this.cartaoCredito = cartaoCredito;
+    }
 
     @Override
     public void imprimirExtrato() {
         System.out.println("=== Extrato Simplificado ===");
-        System.out.println(" -- Conta "+ super.tipo +" --");
+        System.out.println(" -- Conta Corrente --");
         super.imprimirInfosComuns();
+        System.out.println("Cartao de credito: "+ this.cartaoCredito);
+        System.out.println(String.format("Anuidade do cartao: R$ %.2f", this.anuidadeCartao));
+        System.out.println("Data da criacao: " + super.getHoraCriacao().format(Banco.getInstance().formatador));
     }
 }
--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,15 +1,30 @@
 public class ContaPoupanca extends Conta {
+
     public ContaPoupanca(Cliente cliente) {
-        super(cliente,"Poupanca");  //new
+        super(cliente);
     }
     public ContaPoupanca(Cliente cliente, Integer agencia) {
-        super(cliente,"Poupanca", agencia);
+
+        super(cliente,agencia);
+    }
+
+    private Double getRendimento(){
+        Double selicAtual = Banco.getInstance().getTaxaSelic() ;
+
+        if(selicAtual > 8.5)
+            return this.saldo * 0.05;
+        else
+            return this.saldo * (0.7 * selicAtual);
     }
 
     @Override
     public void imprimirExtrato() {
         System.out.println("=== Extrato Simplificado ===");
-        System.out.println(" -- Conta "+ super.tipo +" --");
+        System.out.println(" --- Conta Poupanca ---");
         super.imprimirInfosComuns();
+        System.out.println("Taxa Selic atual: "+ Banco.getInstance().getTaxaSelic() + "%");
+        System.out.println(String.format("Rendimento mensal: R$ %.2f", this.getRendimento()));
+        System.out.println("Data da criacao: " + super.getHoraCriacao().format(Banco.getInstance().formatador));
     }
+
 }
