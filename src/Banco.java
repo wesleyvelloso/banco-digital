007789fa06601ca974b910347e7706a9ef3ea8ff
wesleyvelloso <wmarques092@gmail.com>
2022-05-23 19:14:43 -0300

    Adicionado atributo taxaSelic

--- src/Banco.java
+++ src/Banco.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class Banco { // new - padrao singleton
-
+public class Banco {
     private static Banco instanciaUnica = new Banco();
     private Banco(){
     }
@@ -14,14 +13,18 @@ public class Banco { // new - padrao singleton
         return instanciaUnica;
     }
     private String nome = "SantanDIO";
+
+    private Double taxaSelic = 12.75;
     private List<Conta> contas = new ArrayList<>();
     DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-    public void infoBanco(){  // New
-        System.out.println("=== "+this.nome+" ===");
+    public void infoBanco(){
+        System.out.println("=== " +this.nome+" ===");
         System.out.println("Total de contas: " + contas.size());
         for (Conta conta: this.contas) {
             System.out.println(conta);
         }
     }
+
+
 }
